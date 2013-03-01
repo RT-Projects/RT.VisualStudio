11910d9e7f462af39d3fe849b7bf534b52b49896
Timwi <github@timwi.de>
2013-03-01 03:23:44 +0000

    Reeaaally really fix the comment reformatter. (imported from Perforce changelist 4215)

--- Src/Connect.cs
+++ Src/Connect.cs
@@ -24,13 +24,13 @@ namespace RT.VisualStudio
     /// <seealso class='IDTExtensibility2' />
     public class Connect : IDTExtensibility2, IDTCommandTarget
     {
-        private FontSupport[] _fontsSupported = new FontSupport[] { 
-            new FontSupport { CommandName = "CourierNew", FontName = "Courier New", FontSize = 10 }, 
-            new FontSupport { CommandName = "Candara", FontName = "Candara", FontSize = 11 }, 
-            new FontSupport { CommandName = "SegoeUI", FontName = "Segoe UI", FontSize = 11, UseBold = false }, 
+        private FontSupport[] _fontsSupported = Ut.NewArray(
+            new FontSupport { CommandName = "CourierNew", FontName = "Courier New", FontSize = 10 },
+            new FontSupport { CommandName = "Candara", FontName = "Candara", FontSize = 11 },
+            new FontSupport { CommandName = "SegoeUI", FontName = "Segoe UI", FontSize = 11, UseBold = false },
             new FontSupport { CommandName = "MaiandraGD", FontName = "Maiandra GD kun Eo", FontSize = 11 },
             new FontSupport { CommandName = "Georgia", FontName = "Georgia", FontSize = 11 }
-        };
+        );
 
         private string[] _thingsToBold = new[] { "Keyword", "User Types", "User Types(Value types)", "User Types(Interfaces)", "User Types(Delegates)", "User Types(Enums)", "User Types(Type parameters)" };
         private string[] _platformPriorities = new[] { "x86", "Any CPU", "AnyCPU" };
@@ -165,46 +165,152 @@ namespace RT.VisualStudio
             var endPoint = doc.CreateEditPoint();
             endPoint.EndOfDocument();
             var source = startPoint.GetText(endPoint);
-            var text = Regex.Split(source, @"\r?\n", RegexOptions.Singleline);
+            var resultStr = reformatComments(source);
+            if (resultStr != source)
+                startPoint.ReplaceText(endPoint, resultStr, (int) vsEPReplaceTextOptions.vsEPReplaceTextKeepMarkers);
+        }
+
+        private static string[] _inlineTags = new[] { "see", "paramref", "typeparamref", "c" };
+        private static string[] _blockLevelTags = new[] { "code", "para", "list", "description" };
+
+        private static string reformatComments(string source)
+        {
+            var text = Regex.Split(source.TrimEnd(), @"\r?\n", RegexOptions.Singleline);
             var isComment = text.Select(line => Regex.IsMatch(line, @"^\s*///", RegexOptions.Multiline)).ToArray();
-            var result = new List<string>();
+            var result = new StringBuilder();
+
             foreach (var gr in isComment.GroupConsecutive())
             {
                 if (!gr.Key)
                 {
-                    result.AddRange(text.Skip(gr.Index).Take(gr.Count));
+                    foreach (var line in text.Skip(gr.Index).Take(gr.Count))
+                        result.AppendLine(line);
                     continue;
                 }
 
-                var indentationLength = Regex.Match(text[gr.Index], @"^\s*", RegexOptions.Multiline).Length;
-                var indentation = new string(' ', indentationLength) + "/// ";
-                var wrapWidth = 126 - indentation.Length;
-                var wrap = Ut.Lambda((string str, int width) => str.Trim().WordWrap(width).ToArray());
-                var chunk = XElement.Parse("<item>{0}</item>".Fmt(text.Subarray(gr.Index, gr.Count)
-                    .Select(line => Regex.Replace(line, @"\s*/// ", "", RegexOptions.Multiline))
-                    .JoinString(Environment.NewLine)), LoadOptions.PreserveWhitespace);
-                var elements = chunk.Elements().ToArray();
-                foreach (var elem in elements)
-                    elem.Process();
-
-                // Heuristic: If there is *only* a summary tag, and it fits on one line, make it compact
-                if (elements.Length == 1 && elements[0].Name.LocalName == "summary" && wrap("<summary>" + elements[0].GetContent(), wrapWidth).Length == 1)
-                    result.Add("{0}<summary>{1}</summary>".Fmt(indentation, elements[0].GetContent().Trim()));
-                else
+                try
                 {
-                    var retStr = elements.Select(elem => "{0}{1}{2}</{3}>".Fmt(
-                        elem.GetTag(),
-                        Environment.NewLine,
-                        wrap(elem.GetContent(), wrapWidth - 4).JoinString(Environment.NewLine).Indent(4),
-                        elem.Name.LocalName
-                    )).JoinString(Environment.NewLine);
-                    result.Add(Regex.Replace(retStr, @"^", indentation, RegexOptions.Multiline));
+                    var indentationLength = Regex.Match(text[gr.Index], @"^\s*", RegexOptions.Multiline).Length;
+                    var comment = XElement.Parse(
+                        "<outer>{0}</outer>".Fmt(
+                            text.Skip(gr.Index).Take(gr.Count)
+                                .Select(line => Regex.Replace(line, @"^\s*/// ", "", RegexOptions.Multiline))
+                                .JoinString(Environment.NewLine)
+                        ),
+                        LoadOptions.PreserveWhitespace
+                    );
+
+                    var wrapWidth = 126 - indentationLength;
+                    var indentation = new string(' ', indentationLength) + "/// ";
+
+                    // Special case: single <summary> tag that fits on a line
+                    if (comment.Elements().Count() == 1 && comment.Elements().First().Name.LocalName == "summary" && !comment.Elements().First().Attributes().Any())
+                    {
+                        var summary = reformatComment(comment.Elements().First().Nodes(), false);
+                        var wrapped = summary.WordWrap(wrapWidth - 4);
+                        if (!wrapped.Skip(1).Any())
+                        {
+                            result.Append(indentation);
+                            result.Append("<summary>");
+                            result.Append(summary);
+                            result.AppendLine("</summary>");
+                            continue;
+                        }
+                    }
+
+                    foreach (var line in reformatComment(comment.Nodes(), true).Trim().WordWrap(wrapWidth))
+                        result.AppendLine(indentation + line);
+                }
+                catch (Exception e)
+                {
+                    result.AppendLine("The following comment is not valid: {0}".Fmt(e.Message, e.GetType().Name));
+                    foreach (var line in text.Skip(gr.Index).Take(gr.Count))
+                        result.AppendLine(line);
                 }
             }
 
-            var resultStr = result.JoinString(Environment.NewLine);
-            if (resultStr != source)
-                startPoint.ReplaceText(endPoint, resultStr, (int) vsEPReplaceTextOptions.vsEPReplaceTextKeepMarkers);
+            return result.ToString();
+        }
+
+        private static string reformatComment(IEnumerable<XNode> nodes, bool topLevel, bool keepIndentation = false)
+        {
+            var sb = new StringBuilder();
+
+            var isIn = Ut.Lambda((XNode node, string[] names) => names.Contains(((XElement) node).Name.LocalName));
+
+            if (topLevel || nodes.All(n => (n is XText && string.IsNullOrWhiteSpace(((XText) n).Value)) || (n is XElement && !isIn(n, _inlineTags))) && nodes.Any(n => n is XElement && isIn(n, _blockLevelTags)))
+            {
+                // All tags are block-level or unknown ⇒ use block-level logic, which means:
+                // • Discard all the whitespace between tags
+                // • Put each opening tag on a new line and indent its contents
+                var first = true;
+                foreach (var elem in nodes.OfType<XElement>())
+                {
+                    if (!first)
+                        sb.AppendLine();
+                    first = false;
+                    sb.AppendLine("<{0}{1}>".Fmt(elem.Name.LocalName, elem.Attributes().Select(attr => @" {0}=""{1}""".Fmt(attr.Name.LocalName, attr.Value.HtmlEscape())).JoinString()));
+
+                    string inner;
+                    if (elem.Name.LocalName == "list")
+                    {
+                        // Handle <list> tags specially so that <item> and <description> don’t cause double-indentation
+                        if (!elem.Nodes().All(n => n is XText || (n is XElement && ((XElement) n).Name.LocalName == "item")))
+                            throw new InvalidOperationException("A “list” tag is not supposed to contain anything other than “item” tags.");
+                        inner = elem.Nodes().OfType<XElement>().Select(e => "<item>{0}</item>".Fmt(reformatComment(e.Nodes(), false))).JoinString(Environment.NewLine);
+                    }
+                    else
+                        inner = reformatComment(elem.Nodes(), false, keepIndentation: elem.Name.LocalName == "code");
+
+                    sb.Append(inner.Indent(4));
+                    sb.AppendFormat("</{0}>", elem.Name.LocalName);
+                }
+                return sb.ToString();
+            }
+            else if (nodes.All(n => n is XText || (n is XElement && !isIn(n, _blockLevelTags))) && (!nodes.OfType<XElement>().Any() || nodes.OfType<XElement>().Any(n => isIn(n, _inlineTags))))
+            {
+                var first = true;
+                string lastToAdd = null;
+
+                // All nodes are text, inline-level or unknown ⇒ use inline-level logic, which means:
+                // • Remove all single newlines (but keep double-newlines)
+                // • Put all the nodes inline with the text
+                foreach (var node in nodes)
+                {
+                    var text = node as XText;
+                    var element = node as XElement;
+                    if (text != null)
+                    {
+                        var value = !first ? text.Value : keepIndentation ? text.Value.TrimStart('\r', '\n') : text.Value.TrimStart();
+                        sb.Append(lastToAdd);
+                        if (keepIndentation)
+                            lastToAdd = value;
+                        else
+                            // Replace all “lone” newlines with spaces
+                            lastToAdd = Regex.Replace(value, @"(?<!\n) *\r?\n *(?!\r?\n)", " ");
+                    }
+                    else
+                    {
+                        sb.Append(lastToAdd);
+                        lastToAdd = element.ToString();
+                    }
+                    first = false;
+                }
+                sb.Append(lastToAdd.TrimEnd());
+
+                var result = sb.ToString();
+                var commonIndentation = Regex.Matches(result, @"^ *", RegexOptions.Multiline).Cast<Match>().Min(m => m.Length);
+                return Regex.Replace(result, "^" + new string(' ', commonIndentation), "", RegexOptions.Multiline);
+            }
+            else
+            {
+                var firstBlock = (XElement) nodes.FirstOrDefault(n => n is XElement && isIn(n, _blockLevelTags));
+                var firstInline = (XElement) nodes.FirstOrDefault(n => n is XElement && isIn(n, _inlineTags));
+                if (firstBlock != null && firstInline != null)
+                    throw new InvalidOperationException("“{0}” is block-level, but “{1}” is inline-level.".Fmt(firstBlock.Name.LocalName, firstInline.Name.LocalName));
+                var firstUnknown = (XElement) nodes.FirstOrDefault(n => n is XElement && !isIn(n, _blockLevelTags) && !isIn(n, _inlineTags));
+                throw new InvalidOperationException("I don’t know whether “{0}” is inline-level or block-level.".Fmt(firstUnknown.Name.LocalName));
+            }
         }
 
         private void CreateCommand(string commandName, string readableCommandName, string commandDescription, Action action)
@@ -287,53 +393,6 @@ namespace RT.VisualStudio
                 {
                     _commands[commandName]();
                     handled = true;
-                }
-            }
-        }
-    }
-
-    static class Extensionification
-    {
-        public static string GetContent(this XElement element)
-        {
-            return element.Nodes().Select(n => n.ToString()).JoinString();
-        }
-
-        public static string GetTag(this XElement element)
-        {
-            var sb = new StringBuilder();
-            sb.Append("<");
-            sb.Append(element.Name.LocalName);
-            foreach (var attr in element.Attributes())
-                sb.Append(@" {0}=""{1}""".Fmt(attr.Name.LocalName, attr.Value.HtmlEscape()));
-            sb.Append(">");
-            return sb.ToString();
-        }
-
-        public static void Process(this XElement elem)
-        {
-            var leaveNewLines = elem.Elements("para").Any() || elem.Elements("code").Any() || elem.Elements("list").Any();
-            foreach (var node in elem.Nodes())
-            {
-                if (node is XText && !leaveNewLines)
-                {
-                    var xText = (XText) node;
-                    xText.Value = Regex.Replace(xText.Value, @"\r?\n", " ");
-                }
-                else if (node is XElement)
-                {
-                    var xElement = (XElement) node;
-                    if (xElement.Name.LocalName == "para")
-                        xElement.Process();
-                    else if (xElement.Name.LocalName == "list")
-                    {
-                        foreach (var subElem in xElement.Elements("item"))
-                            foreach (var subElem2 in subElem.Elements("description"))
-                            {
-                                subElem2.Process();
-                                subElem2.FirstNode.AddBeforeSelf(new XText(Environment.NewLine + "    "));
-                            }
-                    }
                 }
             }
         }
